c65d9c4d05636ec2f996bf8d38ab55cc2aaaf331
Marto <martin.romao@hotmail.com>
2021-10-01 23:09:30 -0300

    encaminhamento ao final da tela de cadastro

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
 function verificarPreenchimento(){
-    var username= document.getElementById("loginUsername").value
+    var username= document.getElementById("loginUsername").value;
     if (username= null){
-        console.log("erro, usuário não preenchido")
+        console.log("erro, usuário não preenchido");
     }
 }
