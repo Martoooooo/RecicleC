abf8262d33a50a3d33c59f4962eeda8cd115158e
jhonatan <podearroztkd@gmail.com>
2021-10-05 12:12:16 -0300

    mudanças na validação

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,12 @@
+var lista = [];
 function verificarPreenchimento(){
-    var username= document.getElementById("loginUsername").value
+    var username= document.getElementById("validationDefaultUsername").value;
+    lista.push(username);
     if (username= null){
         console.log("erro, usuário não preenchido")
+    }
+    if(username=username){
+        href="paginas/mapa.html"
+
     }
 }
