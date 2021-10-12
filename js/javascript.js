61c4342d40fe04a49f984ffbf19d18803d3c2baa
Marto <martin.romao@hotmail.com>
2021-10-12 16:02:14 -0300

    Atualização visual

--- js/javascript.js
+++
@@ -1,6 +0,0 @@
-function verificarPreenchimento(){
-    var username= document.getElementById("loginUsername").value
-    if (username= null){
-        console.log("erro, usuário não preenchido")
-    }
-}
