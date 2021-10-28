eb0ef739cbb99938c3263ba10a94b899c586abc4
Marto <martin.romao@hotmail.com>
2021-10-28 18:34:40 -0300

    Atualização visual

--- js/loja.js
+++ js/loja.js
@@ -1,9 +1,39 @@
+function abrirFormulario1(){
+    document.getElementById("fundo").style.display= "block";
+    document.getElementById("dadosVendedor1").style.display = "block";
+}
+function fecharFormulario1(){
+    document.getElementById("fundo").style.display= "none";
+    document.getElementById("dadosVendedor1").style.display = "none";
+
+}
+
 function abrirFormulario(){
     document.getElementById("fundo").style.display= "block";
-    document.getElementById("form").style.display = "block";
+    document.getElementById("dadosVendedor").style.display = "block";
 }
 function fecharFormulario(){
     document.getElementById("fundo").style.display= "none";
-    document.getElementById("form").style.display = "none";
+    document.getElementById("dadosVendedor").style.display = "none";
+
+}
+
+function abrirFormulario(){
+    document.getElementById("fundo").style.display= "block";
+    document.getElementById("dadosVendedor").style.display = "block";
+}
+function fecharFormulario(){
+    document.getElementById("fundo").style.display= "none";
+    document.getElementById("dadosVendedor").style.display = "none";
+
+}
+
+function abrirFormulario(){
+    document.getElementById("fundo").style.display= "block";
+    document.getElementById("dadosVendedor").style.display = "block";
+}
+function fecharFormulario(){
+    document.getElementById("fundo").style.display= "none";
+    document.getElementById("dadosVendedor").style.display = "none";
 
 }
