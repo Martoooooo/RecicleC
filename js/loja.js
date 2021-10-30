9d78b310ce0600051f6d9ecb1abee7d3c1bf24d5
Marto <martin.romao@hotmail.com>
2021-10-30 16:42:11 -0300

    Atualizações visuais

--- js/loja.js
+++ js/loja.js
@@ -1,39 +1,61 @@
+function abrirSenha(){
+    document.getElementById("inputSenhaConfirmacao").style.display= "block";
+}
+function fecharSenha(){
+    document.getElementById("inputSenhaConfirmacao").style.display="none";
+}
+
+
 function abrirFormulario1(){
-    document.getElementById("fundo").style.display= "block";
+    document.getElementById("fundoPopup").style.display= "block";
     document.getElementById("dadosVendedor1").style.display = "block";
 }
 function fecharFormulario1(){
-    document.getElementById("fundo").style.display= "none";
+    document.getElementById("fundoPopup").style.display= "none";
     document.getElementById("dadosVendedor1").style.display = "none";
-
 }
 
-function abrirFormulario(){
-    document.getElementById("fundo").style.display= "block";
-    document.getElementById("dadosVendedor").style.display = "block";
+function abrirFormulario2(){
+    document.getElementById("fundoPopup").style.display= "block";
+    document.getElementById("dadosVendedor2").style.display = "block";
 }
-function fecharFormulario(){
-    document.getElementById("fundo").style.display= "none";
-    document.getElementById("dadosVendedor").style.display = "none";
-
-}
-
-function abrirFormulario(){
-    document.getElementById("fundo").style.display= "block";
-    document.getElementById("dadosVendedor").style.display = "block";
-}
-function fecharFormulario(){
-    document.getElementById("fundo").style.display= "none";
-    document.getElementById("dadosVendedor").style.display = "none";
-
+function fecharFormulario2(){
+    document.getElementById("fundoPopup").style.display= "none";
+    document.getElementById("dadosVendedor2").style.display = "none";
 }
 
-function abrirFormulario(){
-    document.getElementById("fundo").style.display= "block";
-    document.getElementById("dadosVendedor").style.display = "block";
+function abrirFormulario3(){
+    document.getElementById("fundoPopup").style.display= "block";
+    document.getElementById("dadosVendedor3").style.display = "block";
 }
-function fecharFormulario(){
-    document.getElementById("fundo").style.display= "none";
-    document.getElementById("dadosVendedor").style.display = "none";
+function fecharFormulario3(){
+    document.getElementById("fundoPopup").style.display= "none";
+    document.getElementById("dadosVendedor3").style.display = "none";
+}
 
+function abrirFormulario4(){
+    document.getElementById("fundoPopup").style.display= "block";
+    document.getElementById("dadosVendedor4").style.display = "block";
+}
+function fecharFormulario4(){
+    document.getElementById("fundoPopup").style.display= "none";
+    document.getElementById("dadosVendedor4").style.display = "none";
+}
+
+function abrirFormulario5(){
+    document.getElementById("fundoPopup").style.display= "block";
+    document.getElementById("dadosVendedor5").style.display = "block";
+}
+function fecharFormulario5(){
+    document.getElementById("fundoPopup").style.display= "none";
+    document.getElementById("dadosVendedor5").style.display = "none";
+}
+
+function abrirFormulario6(){
+    document.getElementById("fundoPopup").style.display= "block";
+    document.getElementById("dadosVendedor6").style.display = "block";
+}
+function fecharFormulario6(){
+    document.getElementById("fundoPopup").style.display= "none";
+    document.getElementById("dadosVendedor6").style.display = "none";
 }
