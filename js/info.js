e2da2ee8554113507e57b2fba229fdfae4d98eb2
Marto <martin.romao@hotmail.com>
2021-11-02 11:01:06 -0300

    Atualização na página info - encaminhando para fim

---
+++ js/info.js
@@ -0,0 +1,34 @@
+function trocarTipoLixo(){
+    if ($("#listaEscolha").val() == "LE"){
+        document.getElementById("lixoEletronico").style.display="block";
+        document.getElementById("lixoCivil").style.display="none";
+        document.getElementById("lixoOrganico").style.display="none";
+        document.getElementById("lixoToxico").style.display="none";
+
+    } else if($("#listaEscolha").val() == "RCC"){
+        document.getElementById("lixoEletronico").style.display="none";
+        document.getElementById("lixoCivil").style.display="block";
+        document.getElementById("lixoOrganico").style.display="none";
+        document.getElementById("lixoToxico").style.display="none";
+
+    } else if($("#listaEscolha").val() == "LO"){
+        document.getElementById("lixoEletronico").style.display="none";
+        document.getElementById("lixoCivil").style.display="none";
+        document.getElementById("lixoOrganico").style.display="block";
+        document.getElementById("lixoToxico").style.display="none";
+
+    } else if($("#listaEscolha").val() == "LT"){
+        document.getElementById("lixoEletronico").style.display="none";
+        document.getElementById("lixoCivil").style.display="none";
+        document.getElementById("lixoOrganico").style.display="none";
+        document.getElementById("lixoToxico").style.display="block";
+
+    } else if($("#listaEscolha").val() == ""){
+        document.getElementById("lixoEletronico").style.display="none";
+        document.getElementById("lixoCivil").style.display="none";
+        document.getElementById("lixoOrganico").style.display="none";
+        document.getElementById("lixoToxico").style.display="none";
+    }
+
+}
+
