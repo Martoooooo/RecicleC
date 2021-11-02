625a069387882584c03b074846db64c091d3d2c8
Marto <martin.romao@hotmail.com>
2021-11-02 15:04:17 -0300

    Atualização pagina info

--- js/info.js
+++ js/info.js
@@ -4,31 +4,42 @@ function trocarTipoLixo(){
         document.getElementById("lixoCivil").style.display="none";
         document.getElementById("lixoOrganico").style.display="none";
         document.getElementById("lixoToxico").style.display="none";
+        document.getElementById("lixoReciclavel").style.display="none";
 
     } else if($("#listaEscolha").val() == "RCC"){
         document.getElementById("lixoEletronico").style.display="none";
         document.getElementById("lixoCivil").style.display="block";
         document.getElementById("lixoOrganico").style.display="none";
         document.getElementById("lixoToxico").style.display="none";
+        document.getElementById("lixoReciclavel").style.display="none";
 
     } else if($("#listaEscolha").val() == "LO"){
         document.getElementById("lixoEletronico").style.display="none";
         document.getElementById("lixoCivil").style.display="none";
         document.getElementById("lixoOrganico").style.display="block";
         document.getElementById("lixoToxico").style.display="none";
+        document.getElementById("lixoReciclavel").style.display="none";
 
     } else if($("#listaEscolha").val() == "LT"){
         document.getElementById("lixoEletronico").style.display="none";
         document.getElementById("lixoCivil").style.display="none";
         document.getElementById("lixoOrganico").style.display="none";
         document.getElementById("lixoToxico").style.display="block";
+        document.getElementById("lixoReciclavel").style.display="none";
 
-    } else if($("#listaEscolha").val() == ""){
+    } else if($("#listaEscolha").val() == "LR"){
         document.getElementById("lixoEletronico").style.display="none";
         document.getElementById("lixoCivil").style.display="none";
         document.getElementById("lixoOrganico").style.display="none";
         document.getElementById("lixoToxico").style.display="none";
-    }
+        document.getElementById("lixoReciclavel").style.display="block";
 
+    }else if($("#listaEscolha").val() == ""){
+        document.getElementById("lixoEletronico").style.display="none";
+        document.getElementById("lixoCivil").style.display="none";
+        document.getElementById("lixoOrganico").style.display="none";
+        document.getElementById("lixoToxico").style.display="none";
+        document.getElementById("lixoReciclavel").style.display="none";
+    }
 }
 
